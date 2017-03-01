5b3a44de63b0994613291f6a775df47d8bfedcc2
oliver <oliver@oliver-Standard-PC-i440FX-PIIX-1996>
2017-03-01 11:44:22 +0100

    Changes in LanguageToProcessedScriptMapper interface. InterpreterCommand added. Test implementation of LanguageToProcessedScriptMapper removed

--- src/main/java/alexa/calculator/logic/interpreter/LanguageToProcessedScriptMapper.java
+++ src/main/java/alexa/calculator/logic/interpreter/LanguageToProcessedScriptMapper.java
@@ -1,11 +1,13 @@
 package alexa.calculator.logic.interpreter;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 public interface LanguageToProcessedScriptMapper {
 	
-	public ProcessedScript map(String raw);
+	public Collection<List<InterpreterCommand>> map(String raw);
 	
-	public Set<String> supportedCommands();
+	public Set<InterpreterCommand> supportedCommands();
 	
 }
